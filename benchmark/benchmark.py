dfbf47904b94af63c2c89c37601fbc6c5b83dc50
wlxjhyf <xjh1178@gmail.com>
2025-10-16 11:13:25 +0000

    add:add benchmark file

---
+++ benchmark/benchmark.py
@@ -0,0 +1,239 @@
+import torch
+import models.auto_register
+from modelscope import AutoTokenizer, AutoModelForCausalLM
+import os
+import time
+import numpy as np
+import sys
+
+os.environ['FRIC_OFFLOAD'] = '1'
+
+###############################################
+
+model_id = "/home/xujiahao/fric/model"
+
+device = sys.argv[1] if len(sys.argv) > 1 else "cuda"
+CTX_LEN = int(sys.argv[2]) if len(sys.argv) > 2 else 32
+print(f"Device: {device}, CTX_LEN: {CTX_LEN}")
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    dtype=torch.bfloat16,
+).to(device)
+
+raw = open("/home/xujiahao/rwkv/benchmark/eval/calibration_data_v5_rc.txt").read()
+tokens = tokenizer.encode(raw)
+
+messages = [
+    {"role": "system", "content": "You are a kind chatbot."},
+    {"role": "user", "content": "Who are you?"},
+]
+
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(model.device)
+
+generated_tokens = []
+decode_times = []
+prefill_times = []
+
+is_first_time = True
+
+
+def model_run():
+    with torch.inference_mode():
+        t00 = time.perf_counter()
+        outputs = model(input_ids=input_ids, use_cache=True)
+        torch.cuda.synchronize()
+        t1 = time.perf_counter()
+        prefill_times.append(t1 - t00)
+
+        logits = outputs.logits
+        past_key_values = outputs.past_key_values
+        print(type(past_key_values))
+        generated_tokens = []
+           
+    next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+    generated_tokens.append(next_token.item())
+    model.model.fric_offloader.set_last_token_id(next_token)
+
+    for step in range(56):
+        with torch.inference_mode():
+            torch.cuda.synchronize()
+            t0 = time.perf_counter()
+            outputs = model(
+                input_ids=next_token,
+                past_key_values=past_key_values,
+                use_cache=True,
+            )
+            torch.cuda.synchronize()
+            t1 = time.perf_counter()
+            decode_times.append(t1 - t0)
+            logits = outputs.logits
+            past_key_values = outputs.past_key_values
+        
+        next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+        generated_tokens.append(next_token.item())
+        model.model.fric_offloader.set_last_token_id(next_token)
+    
+    response_ids = torch.cat([input_ids, torch.tensor(generated_tokens, device=input_ids.device).unsqueeze(0)], dim=1)
+    print(tokenizer.decode(response_ids[0], skip_special_tokens=True))
+
+def show(times, label="Time"):
+    times = np.array(times)
+    worst = np.max(times)
+    best = np.min(times)
+    percentile_50 = np.percentile(times, 50)
+    mean = np.mean(times)
+    print(f"{label}:")
+    print(f"  Best      : {best * 1000:.3f} ms")
+    print(f"  50% perc  : {percentile_50 * 1000:.3f} ms")
+    print(f"  Mean      : {mean * 1000:.3f} ms")
+    print(f"  Worst     : {worst * 1000:.3f} ms")
+    print("")
+
+def input_prepare(batch_size=1):
+    a = 0
+    nums = 0
+    if batch_size > 1:
+        while a + batch_size * CTX_LEN < len(tokens) and nums < 50:
+            batch_src = [tokens[a + i*CTX_LEN : a + (i+1)*CTX_LEN] for i in range(batch_size)]
+            a += batch_size * CTX_LEN
+
+            messages_list = []
+            for src in batch_src:
+                messages = [
+                    {"role": "system", "content": "You are a kind chatbot."},
+                    {"role": "user", "content": src},
+                ]
+                messages_list.append(messages)
+            input_ids = tokenizer.apply_chat_template(
+                messages_list,
+                add_generation_prompt=True,
+                return_tensors="pt",
+                padding=True,
+                truncation=True
+            ).to(model.device)
+            yield input_ids
+    
+    else:
+        a = 0
+        nums = 0
+        while a + CTX_LEN < len(tokens) and nums < 50:
+            nums += 1
+            src = tokens[a:a+CTX_LEN]
+            messages = [
+                {"role": "system", "content": "You are a kind chatbot."},
+                {"role": "user", "content": src },
+            ]
+            input_ids = tokenizer.apply_chat_template(
+                messages,
+                add_generation_prompt=True,
+                return_tensors="pt"
+            ).to(model.device)
+            yield input_ids
+
+
+def prefill_benchmark(batch_size=1):
+    a = 0
+    nums = 0
+    for input_ids in input_prepare(batch_size):
+        with torch.inference_mode():
+            torch.cuda.synchronize()
+            t00 = time.perf_counter()
+            outputs = model(input_ids=input_ids, use_cache=True)
+            torch.cuda.synchronize()
+            t1 = time.perf_counter()
+            prefill_times.append(t1 - t00)
+
+            global is_first_time
+            if is_first_time is True:
+                print(f"the prefill tokens' number is {outputs.past_key_values[0][0].shape[2]}")
+                is_first_time = False
+        model.model.fric_offloader.offset = 0
+
+def decode_benchmark(batch_size=1):
+    gen = input_prepare(batch_size)
+    input_ids = next(gen)
+    outputs = model(input_ids=input_ids, use_cache=True)
+    logits = outputs.logits
+    next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+    past_key_values = outputs.past_key_values
+    print(type(past_key_values))
+
+    LENGTH_PER_TRIAL = 1
+    for _ in range(LENGTH_PER_TRIAL):
+        torch.cuda.synchronize()
+        t0 = time.perf_counter()
+        outputs = model(
+            input_ids=next_token,
+            past_key_values=past_key_values,
+            use_cache=True,
+        )
+        torch.cuda.synchronize()
+        t1 = time.perf_counter()
+        decode_times.append(t1 - t0)
+        logits = outputs.logits
+        past_key_values = outputs.past_key_values
+        next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+        
+def resume_test():
+    next_token = input_ids
+    past_key_values = None
+    LENGTH_PER_TRIAL1 = 10
+    for _ in range(LENGTH_PER_TRIAL1):
+        outputs = model(
+            input_ids=next_token,
+            past_key_values=past_key_values,
+            use_cache=True,
+        )
+        logits = outputs.logits
+        past_key_values = outputs.past_key_values
+        next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+        generated_tokens.append(next_token.item())
+        model.model.fric_offloader.set_last_token_id(next_token)
+    
+    print("--RESUME TEST: STOP! tokens generated before the resume are below!--")
+    response_ids = torch.cat([input_ids, torch.tensor(generated_tokens, device=input_ids.device).unsqueeze(0)], dim=1)
+    print(tokenizer.decode(response_ids[0], skip_special_tokens=True))
+    generated_tokens.clear()
+    
+    print("--RESUME TEST: CONTINUE! tokens generated after the resume are below!--")
+
+    past_key_values = model.model.fric_offloader.sync_restore()
+
+    LENGTH_PER_TRIAL2 = 47
+    for _ in range(LENGTH_PER_TRIAL2):
+        outputs = model(
+            input_ids=next_token,
+            past_key_values=past_key_values,
+            use_cache=True,
+        )
+        logits = outputs.logits
+        past_key_values = outputs.past_key_values
+        next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+        generated_tokens.append(next_token.item())
+
+    response_ids = torch.tensor(generated_tokens, device=input_ids.device).unsqueeze(0)
+    print(tokenizer.decode(response_ids[0], skip_special_tokens=True))
+
+
+
+if __name__ == "__main__":
+    # model_run()
+
+    # prefill_benchmark(1)
+    # decode_benchmark(1)
+
+    # show(prefill_times, "Prefill Time")
+    # show(decode_times, "Decode Time")
+
+    # if os.environ.get('FRIC_OFFLOAD') == '1':
+    #     model.model.fric_offloader.show()
+
+    # print(model)
+    resume_test()
---
+++ benchmark/riscv_benchmark.py
@@ -0,0 +1,292 @@
+import torch
+import torch_tpu
+
+import sys
+import time
+import os
+from text_generation_server.utils.quantization import get_loader
+from text_generation_server.models.custom_modeling.fric_soph_llama_modeling import (
+    fric_FlashLlamaForCausalLM,
+)
+from text_generation_server.utils import (
+    initialize_torch_distributed_sophon,
+    weight_files,
+    Weights,
+)
+from text_generation_server.layers.attention import KVCache, Seqlen
+
+from transformers import AutoTokenizer, AutoConfig
+
+model_id = "/data/Llama-3.1-8B-Instruct"
+device = sys.argv[1] if len(sys.argv) > 1 else "tpu"
+CTX_LEN = int(sys.argv[2]) if len(sys.argv) > 2 else 32
+dtype = torch.float16
+
+####################  transformer Auto 尝试 ##################
+
+# perfix = ""
+# config = AutoConfig.from_pretrained(
+#     model_id, revision=None, trust_remote_code=False
+# )
+# config.quantize = None
+# config.speculator = None
+# config.sliding_window = None
+# weights_loader = get_loader(quantize = None, model_id = model_id, revision = None)
+# filenames = weight_files(model_id, revision= None, extension=".safetensors")
+# process_group, _, _, _ =  initialize_torch_distributed_sophon()
+# weights = Weights(
+#     filenames,
+#     device,
+#     dtype,
+#     #process_group=torch.distributed.group.WORLD, # None
+#     process_group=process_group,
+#     aliases=None,
+#     weights_loader=weights_loader,
+# )
+
+# tokenizer = AutoTokenizer.from_pretrained(
+#     model_id,
+#     revision=None,
+#     padding_side="left",
+#     truncation_side="left",
+#     trust_remote_code=False,
+# )
+# tokenizer.pad_token = tokenizer.eos_token
+
+# model = fric_FlashLlamaForCausalLM(perfix, config, weights)
+
+
+
+# raw = open("/data/calibration_data_v5_rc.txt").read()
+# tokens = tokenizer.encode(raw)
+
+
+# messages = [
+#     {"role": "system", "content": "You are a kind chatbot."},
+#     {"role": "user", "content": "Who are you?"},
+# ]
+
+# input_ids = tokenizer.apply_chat_template(
+#     messages,
+#     add_generation_prompt=True,
+#     return_tensors="pt"
+# ).to(device)
+
+# generated_tokens = []
+# decode_times = []
+# prefill_times = []
+
+# is_first_time = True
+
+
+# def model_run():
+#     t00 = time.perf_counter()
+#     logits, speculative_logits = model(input_ids=input_ids)
+#     torch.tpu.synchronize()
+#     t1 = time.perf_counter()
+#     prefill_times.append(t1 - t00)
+
+#     past_key_values = model.kv_cache
+#     generated_tokens = []
+           
+#     next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+#     generated_tokens.append(next_token.item())
+#     # model.model.fric_offloader.set_last_token_id(next_token)
+
+#     for step in range(56):
+#         torch.tpu.synchronize()
+#         t0 = time.perf_counter()
+#         logits, _ = model(
+#             input_ids = next_token,
+#             kv_cacahe = past_key_values,
+#         )
+#         torch.tpu.synchronize()
+#         t1 = time.perf_counter()
+#         decode_times.append(t1 - t0)
+
+#         past_key_values = model.kv_cache
+
+#         next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(1)
+#         generated_tokens.append(next_token.item())
+#         # model.model.fric_offloader.set_last_token_id(next_token)
+    
+#     response_ids = torch.cat([input_ids, torch.tensor(generated_tokens, device=input_ids.device).unsqueeze(0)], dim=1)
+#     print(tokenizer.decode(response_ids[0], skip_special_tokens=True))
+
+
+
+
+######### init ########
+# forward需要很多参数，而这些参数大多在上一层被初始化
+# 为了代码的简便性，坚决不用上层函数和batch
+# 那么就需要对这些参数的生命周期进行管理
+#'position_ids', 'cu_seqlen_prefill', 'kv_cache', 'block_tables', 'slots', 'seqlen', and 'max_s'
+
+# 换策略，使用batch，但是不用generate
+# 0层LM class，forward输入是batch，返回是logits
+# 故我还需要去明白 logits 到 batch 的转换
+
+
+# import math
+# import soph_config
+# from text_generation_server.models.globals import BLOCK_SIZE
+# DECODE_TOKEN_LEN = soph_config.DECODE_TOKEN_LEN
+# CONTEXT_LEN = soph_config.CONTEXT_LEN
+# batches = 1
+# chat_token_num = 27
+# num_blocks = max(1024, (math.ceil((CONTEXT_LEN + DECODE_TOKEN_LEN + chat_token_num) / BLOCK_SIZE) * batches))
+# cache_manager = model.init_kv_cache(
+#     num_blocks,
+#     model.num_layers,
+#     model.num_kv_heads,
+#     model.head_size,
+#     model.dtype,
+#     model.device,
+# )
+# position_ids = None
+# cu_seqlen_prefill = None
+
+# cache_lengths_tensor=None
+# input_lengths_tensor=None
+
+# # block_tables = 
+# # slots = 
+
+# # seqlen = Seqlen(
+# #     input_lengths=input_lengths,
+# #     cache_lengths=cache_lengths_tensor,
+# #     cu_seqlen_q=cu_seqlen_prefill,
+# #     max_q=batch.max_input_length,
+# #     max_k=batch.max_current_length,
+# # )
+
+# # seqlen = 
+# # max_s = 
+
+
+#########################################################
+
+# 先给出一版采用generate完成推理的代码 （妥协
+# 但还是要摸清这部分关于CPU和GPU的控制，如果有很多的sync操作，要摸清其中的必要性
+# 即排除serve之外的内容
+
+# ----------- prepare ---------- #
+from text_generation_server.models import Model, get_model
+from text_generation_server.pb import generate_pb2
+from text_generation_server.models.flash_causal_lm import FlashCausalLMBatch
+import math
+import soph_config
+from text_generation_server.models.globals import BLOCK_SIZE
+import numpy as np
+
+
+def llama_chat_wrapper(question):
+    return f"<s>[INST] <<SYS>>\nYou are a helpful assistant.\n<</SYS>>\n\n{question}[/INST]"
+
+
+def default_pb_parameters():
+    return generate_pb2.NextTokenChooserParameters(
+        temperature=1.0,
+        repetition_penalty=1.0,
+        top_k=0,
+        top_p=1.0,
+        typical_p=1.0,
+        do_sample=False,
+    )
+
+def default_pb_stop_parameters(max_new_tokens=100):
+    return generate_pb2.StoppingCriteriaParameters(
+        stop_sequences=[], max_new_tokens=max_new_tokens
+    )
+
+def defatult_multi_batch_pb(batches, mode):
+    questions = [
+        "What is Deep Learning?",
+    ] * batches
+    question_length = [5] * batches
+
+    if mode == "chat":
+        questions = [llama_chat_wrapper(question + ". " * max(0, CONTEXT_LEN - qlen - 27)) for qlen, question in zip(question_length, questions)]
+
+    requests = []
+    for i in range(batches):
+        requests.append(
+            generate_pb2.Request(
+                id=i,
+                inputs=questions[i],
+                input_chunks=generate_pb2.Input(
+                    chunks=[generate_pb2.InputChunk(text=questions[i])]
+                ),
+                prefill_logprobs=False,
+                truncate=CONTEXT_LEN + 50,
+                parameters=default_pb_parameters(),
+                stopping_parameters=default_pb_stop_parameters(DECODE_TOKEN_LEN),
+            )
+        )
+    batch_pb = generate_pb2.Batch(id=1, requests=requests, size=batches)
+    return batch_pb
+
+
+DECODE_TOKEN_LEN = soph_config.DECODE_TOKEN_LEN
+CONTEXT_LEN = soph_config.CONTEXT_LEN
+batches = 1
+chat_token_num = 27
+mode="chat"
+num_blocks = max(1024, (math.ceil((CONTEXT_LEN + DECODE_TOKEN_LEN + chat_token_num) / BLOCK_SIZE) * batches))
+
+
+model = get_model(
+    model_id=model_id,
+    lora_adapter_ids=[],
+    revision=None,
+    sharded=False,
+    quantize=None,
+    speculate=None,
+    dtype="float16",
+    kv_cache_dtype=None,
+    trust_remote_code=False,
+    max_input_tokens=512,
+)
+
+cache_manager = model.init_kv_cache(
+    num_blocks,
+    model.num_layers,
+    model.num_kv_heads,
+    model.head_size,
+    model.dtype,
+    model.device,
+)
+
+
+def model_run():
+    time_list = []
+    generated_text = {}
+    for i in range(DECODE_TOKEN_LEN):
+        os.environ["TOKEN_IDX"] = str(i)
+
+        generate_start = time.time_ns()
+        generations, next_batch, (forward_ns, decode_ns) = model.generate_token(
+                next_batch
+        )
+        generate_end = time.time_ns()
+        
+        time_list.append(generate_end - generate_start)
+
+        for generation in generations:
+            if i == 0:
+                generated_text[generation.request_id] = generation.tokens.texts[0]
+            else:
+                generated_text[generation.request_id] += generation.tokens.texts[0]
+        
+        if next_batch is None:
+                break
+        
+
+    for key in generated_text.keys():
+        print(f"Batch {key}: {generated_text[key]}")
+        print(f"FTL: {time_list[0] / 1000**2:.1f}ms, TPS: {batches / np.mean(time_list[1:]) * 1000**3:.1f}")
+        print(f'TTFT: {time_list[0] /1000**3:.3f}s, TPOT: {np.mean(time_list[1:]) /1000**3:.3f}s, Throughput: {batches / np.mean(time_list[1:]) *1000**3:.1f}, TPS: {1/ np.mean(time_list[1:]) *1000**3:.1f}')
+
+
+if __name__ == "__main__":
+    model_run()
